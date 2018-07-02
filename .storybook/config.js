1167f1e63e7b87609ce1ec4880fd87d5e9e9b57c
c3dr0x <cedric.bouletkessler@gmail.com>
2018-07-02 23:39:00 +0200

    feat: Add material-icons to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure, addDecorator } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 
 import "material-components-web/dist/material-components-web.css";
+import "material-design-icons/iconfont/material-icons.css";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src/components", true, /.stories.tsx$/);
