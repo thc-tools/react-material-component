6a721284c8bfe12f6be4a14cee971d543ee0ec8f
c3dr0x <cedric.bouletkessler@gmail.com>
2018-07-03 22:22:42 +0200

    feat: Add some custom style to stprybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,13 +3,12 @@ import { withInfo } from "@storybook/addon-info";
 
 import "material-components-web/dist/material-components-web.css";
 import "material-design-icons/iconfont/material-icons.css";
+import "./custom.css";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src/components", true, /.stories.tsx$/);
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
-
-addDecorator(withInfo({ inline: true, header: false }));
 
 configure(loadStories, module);
