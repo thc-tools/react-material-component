b5e130e87269cc10e34a0d2cea37c806e9f09bc3
c3dr0x <cedric.bouletkessler@gmail.com>
2018-07-13 22:42:01 +0200

    fix(test): Fix Jest config, by adding babel transform and config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
 module.exports = {
     roots: ["<rootDir>/src"],
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.jsx?$": "babel-jest"
     },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 
     // Setup Enzyme
     snapshotSerializers: ["enzyme-to-json/serializer"],
-    setupTestFrameworkScriptFile: "<rootDir>/enzyme.config.js"
+    setupTestFrameworkScriptFile: "<rootDir>/enzyme.config.js",
+    transformIgnorePatterns: [
+        "node_modules/(?!(@material|my-project)/)"    
+    ]
 };
